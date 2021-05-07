6a48f4a833c2e6a7070de89067c7d3913c0f1500
Lucas Brito <lucasbsilva29@gmail.com>
2021-05-07 17:26:55 -0300

    feat(main.py): making high pass filter from low pass

--- ST4/src/main.py
+++ ST4/src/main.py
@@ -9,7 +9,12 @@ def main():
     M = 5
     cutoff_freq = 1_500
     samples_p_second = 24_000
-    low_pas_filter = create_low_pass_filter(M, cutoff_freq, samples_p_second)
+    low_pass_filter = create_low_pass_filter(M, cutoff_freq, samples_p_second)
+    high_pass_filter = to_high_pass_filter(M, low_pass_filter)
+    print(
+        'Low-pass filter: {0}\nHigh-pass filter: {1}'.format(
+            low_pass_filter, high_pass_filter)
+    )
 
 
 def create_low_pass_filter(size, passing_freq, samples_sec):
@@ -36,6 +41,17 @@ def create_low_pass_filter(size, passing_freq, samples_sec):
         )
 
     return filter
+
+
+def to_high_pass_filter(M, low_pass_filter: list):
+    if (M % 2) == 0:
+        raise ValueError('''You can't use even magnetude.''')
+    low_pass_filter = np.array(low_pass_filter)
+    low_pass_filter = low_pass_filter[::-1]
+    result = [None]*(len(low_pass_filter))
+    result[::2] = low_pass_filter[::2]
+    result[1::2] = low_pass_filter[1::2] * -1
+    return result
 
 
 if __name__ == '__main__':

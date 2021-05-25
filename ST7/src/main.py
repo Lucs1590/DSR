267b9b5212f42d43b2db9665575b8828e178deff
Lucas Brito <lucasbsilva29@gmail.com>
2021-05-24 22:24:13 -0300

    docs(main.py): making an initial main

---
+++ ST7/src/main.py
@@ -0,0 +1,15 @@
+from scipy.io import wavfile as wv
+from functools import reduce
+import numpy as np
+import math
+
+
+def main():
+    """ # Main
+    This is a backbone of the project. This Fuction runs all the others.
+    """
+    ...
+
+
+if __name__ == '__main__':
+    main()

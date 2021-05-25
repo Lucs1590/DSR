29f5ebb166e36efa10f69ae77f8ce815db17d8c4
Lucas Brito <lucasbsilva29@gmail.com>
2021-05-25 00:18:45 -0300

    docs(main.py): making a draft

--- ST7/src/main.py
+++ ST7/src/main.py
@@ -10,11 +10,24 @@ def main():
     amplitute_variation = [1.01, 0.99]
     frequence_variation = [0.06, 0]
     transition_band = [(0.45*math.pi), (0.15*math.pi)]
-
+    # - fuction to set diffs
+    # (passband, stopband, transition_band_diff) = set_diffs(amplitute_variation, frequence_variation, transition_band)
     passband = np.diff(amplitute_variation)[0]
     stopband = np.diff(frequence_variation)[0]
+    transition_band_diff = np.diff(transition_band)[0]
+
     omega_c = np.mean(transition_band)
+    # - dB transform
+    # - set min flutuation 1:47:14
+    # - choose window type
+    # - set magnetude
+    # - create filter
+    M = get_magnetude(transition_band_diff)
     print('test')
+
+
+def get_magnetude(transition_band_diff):
+    ...
 
 
 if __name__ == '__main__':

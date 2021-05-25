10f2d5a9017b9acc483a7aba7a9a168cf3730cfb
Lucas Brito <lucasbsilva29@gmail.com>
2021-05-25 11:09:30 -0300

    feat(main.py): add window selector

--- ST7/src/main.py
+++ ST7/src/main.py
@@ -6,9 +6,9 @@ def main():
     """ # Main
     This is a backbone of the project. This Fuction runs all the others.
     """
-    amplitute_variation = [1.01, 0.99]
-    frequency_variation = [0.06, 0]
-    transition_band = [(0.45*math.pi), (0.15*math.pi)]
+    amplitute_variation = [0.99, 1.01]
+    frequency_variation = [0, 0.06]
+    transition_band = [(0.15*math.pi), (0.45*math.pi)]
     (passband, stopband, transition_band_diff) = set_diffs(
         amplitute_variation, frequency_variation, transition_band)
     omega_c = np.mean(transition_band)
@@ -16,7 +16,7 @@ def main():
     dB = to_dB(stopband)
     # - set min flutuation 1:47:14
     # - choose window type
-    flutuation_type = chose_window_type(dB)
+    flutuation_type = choose_window_type(dB)
     # - set magnetude
     M = get_magnetude(transition_band_diff)
     # - create filter
@@ -36,8 +36,19 @@ def to_dB(frequency, base=20):
     return base * math.log10(frequency)
 
 
-def chose_window_type(frequency):
-    ...
+def choose_window_type(measure):
+    reference = {
+        'rectangular': -21,
+        'barlett': -25,
+        'hanning': -44,
+        'hamming': -53,
+        'blackman': -74
+    }
+    allow_values = list(
+        filter(lambda value: value[1] < measure, reference.items()))
+    closet_value = sorted(list(map(lambda value: (value[0], abs(
+        value[1]-measure)), allow_values)), key=lambda element: element[1])
+    return closet_value[0][0]
 
 
 def get_magnetude(transition_band_diff):

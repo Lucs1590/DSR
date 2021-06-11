d3e7b8bdb3d83df9f462069af86f6ac9ce0c041e
Lucas Brito <lucasbsilva29@gmail.com>
2021-06-11 19:38:32 -0300

    feat(main.py): delay and start audio slip

--- ST10/src/main.py
+++ ST10/src/main.py
@@ -12,6 +12,31 @@ def main():
     samples_second = 8_000
     ms = 0.25
 
+    delay_spaces = define_delay(samples_second, ms)
+    (energy, zcr) = audio_slip(raw_audio, delay_spaces)
+
+
+def define_delay(ss, ms):
+    return int((ss * ms)/1000)
+
+
+def audio_slip(signal, delay, overlap=0.5):
+    signal = np.array(signal)
+    energy_list = []
+    aux = []
+    zc = 0
+    i = 0
+
+    while i < len(signal):
+        values = signal[i:i + delay]
+        energy_list.append()
+        i += int(i / overlap)
+
+    return aux
+
+
+def calc_energy(frame):
+    return list(map())
 
 if __name__ == '__main__':
     main()

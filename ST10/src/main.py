ca0426242717f72c00d3bfef4b285f4fd53cb81c
Lucas Brito <lucasbsilva29@gmail.com>
2021-06-11 16:13:16 -0300

    feat(main.py): set params

---
+++ ST10/src/main.py
@@ -0,0 +1,17 @@
+from scipy.io import wavfile as wv
+from functools import reduce
+import numpy as np
+import math
+
+
+def main():
+    """ # Main
+    This is a backbone of the project. This Fuction runs all the others.
+    """
+    raw_audio = [1, 2, -3, 3, -2, 1, -1, -1, 4, 5, -5, 4]
+    samples_second = 8_000
+    ms = 0.25
+
+
+if __name__ == '__main__':
+    main()

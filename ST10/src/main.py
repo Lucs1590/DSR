04bfa2c9f2606cd99ab79366e38094a06bceac23
Lucas Brito <lucasbsilva29@gmail.com>
2021-06-13 17:01:51 -0300

    feat(main.py): compleating energy and zcr

--- ST10/src/main.py
+++ ST10/src/main.py
@@ -23,20 +23,30 @@ def define_delay(ss, ms):
 def audio_slip(signal, delay, overlap=0.5):
     signal = np.array(signal)
     energy_list = []
-    aux = []
-    zc = 0
+    zcr_list = []
     i = 0
 
     while i < len(signal):
         values = signal[i:i + delay]
-        energy_list.append()
+        energy_list.append(calc_energy(values))
+        zcr_list.append(calc_zcr(values))
         i += int(i / overlap)
 
-    return aux
+    return (energy_list, zcr_list)
 
 
 def calc_energy(frame):
-    return list(map())
+    return np.sum(np.power(frame, 2))
+
+
+def calc_zcr(frame):
+    i = 0
+    aux = 0
+    while i < len(frame):
+        aux += 1 if (frame[i] * frame[i+1]) < 0 else 0
+        i += 1
+    return aux
+
 
 if __name__ == '__main__':
     main()

17f7cdafe11f17cb1177da77511410435e250835
Lucas Brito <lucasbsilva29@gmail.com>
2021-07-09 18:31:47 -0300

    feat(main.py): making a backbone

---
+++ ST12/src/main.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def main():
+    """ # Main
+    This is a backbone of the project. This Fuction runs all the others.
+    """
+    order = 3
+    signal = [2, 3, 5, 8, 8, 10, 6]
+    signal = complete_with_zero(signal, order)
+    (matrix_A, matrix_B) = define_matrix_A_B(signal)
+
+
+def complete_with_zero(signal, order):
+    ...
+
+
+def define_matrix_A_B(signal):
+    ...
+
+
+if __name__ == '__main__':
+    main()

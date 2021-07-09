ca030ffcab8c68e24ec90e41b8847d40b855adba
Lucas Brito <lucasbsilva29@gmail.com>
2021-07-09 19:28:39 -0300

    feat(main.py): set matrix A and B

--- ST12/src/main.py
+++ ST12/src/main.py
@@ -9,7 +9,7 @@ def main():
     order = 3
     signal = [2, 3, 5, 8, 8, 10, 6]
     signal = complete_with_zeros(signal, order)
-    (matrix_A, matrix_B) = define_matrix_A_B(signal)
+    (matrix_A, matrix_B) = define_matrix_A_B(signal, order)
 
 
 def complete_with_zeros(signal, order):
@@ -20,8 +20,16 @@ def complete_with_zeros(signal, order):
     return signal
 
 
-def define_matrix_A_B(signal):
-    ...
+def define_matrix_A_B(signal, order):
+    matrix_a = []
+    matrix_b = []
+    i = 0
+
+    for i in range(len(signal) - order):
+        matrix_a.append(signal[i:i+order])
+        matrix_b.append(signal[i+order])
+
+    return matrix_a, matrix_b
 
 
 if __name__ == '__main__':

90f95433055ab803979c071f7c897d89913e9568
Lucas Brito <lucasbsilva29@gmail.com>
2021-07-10 13:53:47 -0300

    docs(main.py): making the methods docs

--- ST12/src/main.py
+++ ST12/src/main.py
@@ -16,6 +16,15 @@ def main():
 
 
 def complete_with_zeros(signal, order):
+    """# Complete with zeros
+
+    Args:
+        signal (list): list with audio samples.
+        order (int): window size.
+
+    Returns:
+        list: list with the zeros after and before.
+    """
     for i in range(order):
         signal.append(0)
         signal.insert(0, 0)
@@ -24,6 +33,15 @@ def complete_with_zeros(signal, order):
 
 
 def define_matrix_A_B(signal, order):
+    """ # Define matriz
+
+    Args:
+        signal (list): list with audio samples.
+        order (int): window size.
+
+    Returns:
+        tuple: two matrix with equation incognitas and results
+    """
     matrix_a = []
     matrix_b = []
     i = 0
@@ -36,6 +54,15 @@ def define_matrix_A_B(signal, order):
 
 
 def make_operations(matrix_A, matrix_B):
+    """ # Make Operations
+
+    Args:
+        matrix_A (numpy.ndarray): list with equations values
+        matrix_B (numpy.ndarray): list with results values
+
+    Returns:
+        tuple: initial matrix mutiplied by first matrix transposed
+    """
     matrix_A_transp = matrix_A.transpose()
     mAtA = np.dot(matrix_A_transp, matrix_A)
     mAtB = np.dot(matrix_A_transp, matrix_B)
@@ -43,10 +70,26 @@ def make_operations(matrix_A, matrix_B):
 
 
 def make_coefficients(matrix_A, matrix_B):
+    """ # Make Coefficients
+
+    Args:
+        matrix_A (numpy.ndarray): first matrix multiplied by itself transposed
+        matrix_B (numpy.ndarray): second matrix multiplied by first one transposed
+
+    Returns:
+        numpy.ndarray: the coefficients of linear equation
+    """
     return np.linalg.solve(matrix_A, matrix_B)
 
 
 def show_results(matrix_A, matrix_B, coefficients):
+    """ # Show Results
+
+    Args:
+        matrix_A (numpy.ndarray): first matrix multiplied by itself transposed
+        matrix_B (numpy.ndarray): second matrix multiplied by first one transposed
+        coefficients (numpy.ndarray): the coefficients of linear equation
+    """
     print("\n{0} * {1} = {2}\n\n{3}\n".format(
         matrix_A,
         np.array(['a{0}'.format(i+1) for i, j in enumerate(coefficients)]),
